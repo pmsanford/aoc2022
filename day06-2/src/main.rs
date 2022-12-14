78d5136c2633c2d471ff4385ae9c1a7a9f1147af
Paul Sanford <me@paulsanford.net>
2022-12-13 23:20:51 -0700

    day6.2

---
+++ day06-2/src/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashSet;
+
+use anyhow::Result;
+use util::Input;
+
+fn main() -> Result<()> {
+    let input = Input::new()?.into_string()?.chars().collect::<Vec<_>>();
+
+    let indicator_len = 14;
+
+    for idx in indicator_len..input.len() {
+        if HashSet::<&char>::from_iter(input[idx - indicator_len..idx].iter()).len()
+            == indicator_len
+        {
+            println!(r"Idx: {idx}");
+            break;
+        }
+    }
+
+    Ok(())
+}
